847081f210285b88e792d1a346b72f1b2d86506a
Malte Ubl <malte.ubl@gmail.com>
2011-12-01 18:23:12 +0100

    Rough implementation of name resolving

--- test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
+++ test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
@@ -26,7 +26,7 @@ public class ProcessCommonJSModulesTest extends CompilerTestCase {
   }
 
   @Override protected CompilerPass getProcessor(Compiler compiler) {
-    return new ProcessCommonJSModules(compiler, "foo/bar/");
+    return new ProcessCommonJSModules(compiler, "foo/bar/", false);
   }
 
   @Override
@@ -40,15 +40,28 @@ public class ProcessCommonJSModulesTest extends CompilerTestCase {
     setFilename("test/sub");
     test("var name = require('mod/name'); (function() { name(); })();", "goog.provide('module$test$sub'); var module$test$sub = {}; goog.require('module$mod$name'); var name$$module$test$sub = module$mod$name; (function() { name$$module$test$sub(); })();if(module$test$sub.module$exports)module$test$sub=module$test$sub.module$exports");
   }
-  
+
   public void testExports() {
     setFilename("test");
     test("var name = require('name'); exports.foo = 1;", "goog.provide('module$test'); var module$test = {}; goog.require('module$name'); var name$$module$test = module$name; module$test.foo = 1;if(module$test.module$exports)module$test=module$test.module$exports");
     test("var name = require('name'); module.exports = function() {};", "goog.provide('module$test'); var module$test = {}; goog.require('module$name'); var name$$module$test = module$name; module$test.module$exports = function() {};if(module$test.module$exports)module$test=module$test.module$exports");
   }
-  
+
   public void testVarRenaming() {
     setFilename("test");
     test("var a = 1, b = 2; (function() { var a; b = 4})()", "goog.provide('module$test'); var module$test = {}; var a$$module$test = 1, b$$module$test = 2; (function() { var a; b$$module$test = 4})(); if(module$test.module$exports)module$test=module$test.module$exports");
+  }
+
+  public void testModuleName() {
+    setFilename("foo/bar");
+    test("var name = require('name');",
+        "goog.provide('module$foo$bar'); var module$foo$bar = {}; goog.require('module$name'); var name$$module$foo$bar = module$name; if(module$foo$bar.module$exports)module$foo$bar=module$foo$bar.module$exports");
+    assertEquals("module$foo$bar$baz", ProcessCommonJSModules.toModuleName("./baz.js", "foo/bar.js"));
+    assertEquals("module$foo$baz", ProcessCommonJSModules.toModuleName("../baz.js", "foo/bar.js"));
+    assertEquals("module$baz", ProcessCommonJSModules.toModuleName("../../baz.js", "foo/bar.js"));
+    assertEquals("module$baz", ProcessCommonJSModules.toModuleName("../../../baz.js", "foo/bar/baz.js"));
+    test("var name = require('./name');",
+        "goog.provide('module$foo$bar'); var module$foo$bar = {}; goog.require('module$foo$bar$name'); var name$$module$foo$bar = module$foo$bar$name; if(module$foo$bar.module$exports)module$foo$bar=module$foo$bar.module$exports");
+
   }
 }
