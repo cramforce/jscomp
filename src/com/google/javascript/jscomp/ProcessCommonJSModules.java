a01edb8e3750d8fc39164a4637e9c0e253307405
Malte Ubl <malte.ubl@gmail.com>
2011-12-03 15:22:31 +0100

    Lots of cleanup

--- src/com/google/javascript/jscomp/ProcessCommonJSModules.java
+++ src/com/google/javascript/jscomp/ProcessCommonJSModules.java
@@ -15,37 +15,40 @@
  */
 package com.google.javascript.jscomp;
 
-import java.io.File;
 import java.util.List;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
-import com.google.javascript.jscomp.deps.SortedDependencies;
-import com.google.javascript.jscomp.deps.SortedDependencies.CircularDependencyException;
 import com.google.javascript.rhino.IR;
-import com.google.javascript.rhino.InputId;
 import com.google.javascript.rhino.Node;
 
 /**
- *
+ * Rewrites a Common JS module http://wiki.commonjs.org/wiki/Modules/1.1.1
+ * into a form that can be safely concatenated.
+ * Does not add a function around the module body but instead adds suffixes
+ * to global variables to avoid conflicts.
+ * Calls to require are changed to reference the required module directly.
+ * goog.provide and goog.require are emitted for closure compiler automatic
+ * ordering.
  */
 class ProcessCommonJSModules implements CompilerPass {
+
+  private static final String MODULE_NAME_PREFIX = "module$";
 
   private final AbstractCompiler compiler;
   private final String filenamePrefix;
   private final boolean reportDependencies;
-  private List<CompilerInput> inputs;
   private JSModule module;
 
-  ProcessCommonJSModules(AbstractCompiler compiler, String filenamePrefix, List<CompilerInput> inputs) {
+  ProcessCommonJSModules(AbstractCompiler compiler, String filenamePrefix) {
     this.compiler = compiler;
     this.filenamePrefix = filenamePrefix;
     this.reportDependencies = true;
-    this.inputs = inputs;
   }
 
-  ProcessCommonJSModules(AbstractCompiler compiler, String filenamePrefix, boolean reportDependencies) {
+  ProcessCommonJSModules(AbstractCompiler compiler, String filenamePrefix,
+      boolean reportDependencies) {
     this.compiler = compiler;
     this.filenamePrefix = filenamePrefix;
     this.reportDependencies = reportDependencies;
@@ -53,55 +56,54 @@ class ProcessCommonJSModules implements CompilerPass {
 
   @Override
   public void process(Node externs, Node root) {
-    NodeTraversal.traverse(compiler, root, new ProcessCommonJsModulesCallback());
+    NodeTraversal
+        .traverse(compiler, root, new ProcessCommonJsModulesCallback());
   }
 
   private String guessCJSModuleName(String filename) {
     return toModuleName(normalizeSourceName(filename));
   }
 
-  public boolean hasCyclicDependency() {
-    try {
-      new SortedDependencies(inputs);
-    } catch (CircularDependencyException e) {
-      return true;
-    }
-    return false;
-  }
-
   public JSModule getModule() {
     return module;
   }
 
+  /**
+   * Turns a filename into a JS identifier that is used for moduleNames in
+   * rewritten code. Removes leading ./, replaces / with $, removes trailing .js
+   * and replaces - with _. All moduleNames get a "module$" prefix.
+   */
   public static String toModuleName(String filename) {
-    return "module$" + filename.
-        replaceAll("^\\.\\/", "").
-        replaceAll("\\/", "\\$").
-        replaceAll("\\.js$", "").
-        replaceAll("-", "_");
+    return MODULE_NAME_PREFIX +
+        filename.replaceAll("^\\.\\/", "").replaceAll("\\/", "\\$")
+            .replaceAll("\\.js$", "").replaceAll("-", "_");
   }
 
+  /**
+   * Turn a filename into a moduleName with support for relative addressing
+   * with ./ and ../ based on currentFilename;
+   */
   public static String toModuleName(String filename, String currentFilename) {
     filename = filename.replaceAll("\\.js$", "");
     currentFilename = currentFilename.replaceAll("\\.js$", "");
 
-    int dirsBack = 0;
-    while (filename.startsWith("./")) {
-      filename = filename.substring(2);
-      dirsBack++;
-    }
-    if (filename.startsWith("../")) {
-      dirsBack++;
-    }
-    while (filename.startsWith("../")) {
-      filename = filename.substring(3);
-      dirsBack++;
-    }
+    // More elegant algorithms welcome but it is late.
+    boolean relative = false;
     List<String> parts = Lists.newArrayList(currentFilename.split("/"));
-    if (dirsBack > 0) {
-      for (int i = 0; i < dirsBack; i++) {
+    if (filename.startsWith("./")) {
+      filename = filename.substring(2);
+      parts.remove(parts.size() - 1);
+      relative = true;
+    }
+    else if (filename.startsWith("../")) {
+      parts.remove(parts.size() - 1);
+      while (filename.startsWith("../")) {
+        filename = filename.substring(3);
         parts.remove(parts.size() - 1);
       }
+      relative = true;
+    }
+    if (relative) {
       parts.add(filename);
       filename = Joiner.on("/").join(parts);
     }
@@ -116,75 +118,113 @@ class ProcessCommonJSModules implements CompilerPass {
   }
 
   /**
+   * Visits require, every "script" and special module.exports assignments.
    */
-  private class ProcessCommonJsModulesCallback
-      extends AbstractPostOrderCallback {
+  private class ProcessCommonJsModulesCallback extends
+      AbstractPostOrderCallback {
 
     @Override
     public void visit(NodeTraversal t, Node n, Node parent) {
-      if (n.isCall() && n.getFirstChild() != null &&
-          n.getChildCount() == 2 &&
-          n.getFirstChild().isName() && "require".equals(n.getFirstChild().getString()) &&
+      if (n.isCall() && n.getFirstChild() != null && n.getChildCount() == 2 &&
+          n.getFirstChild().isName() &&
+          "require".equals(n.getFirstChild().getString()) &&
           n.getChildAtIndex(1).isString()) {
-        String moduleName = toModuleName(n.getChildAtIndex(1).getString(), normalizeSourceName(t.getSourceName()));
-        Node moduleRef = IR.name(moduleName).srcref(n);
-        parent.replaceChild(n, moduleRef);
-        Node script = getCurrentScriptNode(parent);
-        //System.out.println("Require " + moduleName + " in " + guessCJSModuleName(t.getSourceName()));
-        if (reportDependencies) {
-          compiler.getInput(script.getInputId()).addRequire(moduleName);
-          /*if (hasCyclicDependency()) {
-            compiler.getInput(script.getInputId()).removeRequire(moduleName);
-          }*/
-        }
-        script.addChildToFront(IR.exprResult(
-            IR.call(IR.getprop(IR.name("goog"), IR.string("require")), IR.string(moduleName))
-        ).copyInformationFromForTree(n));
-        compiler.reportCodeChange();
+        visitRequireCall(t, n, parent);
       }
 
       if (n.isScript()) {
-        String moduleName = guessCJSModuleName(normalizeSourceName(n.getSourceFileName()));
-        n.addChildToFront(IR.var(IR.name(moduleName), IR.objectlit()).copyInformationFromForTree(n));
-        // System.out.println("Provide " + moduleName);
-        if (reportDependencies) {
-          CompilerInput ci = compiler.getInput(n.getInputId());
-          ci.addProvide(moduleName);
-          JSModule m = new JSModule(moduleName);
-          m.add(ci);
-          module = m;
-        }
-        n.addChildToFront(IR.exprResult(
-            IR.call(IR.getprop(IR.name("goog"), IR.string("provide")), IR.string(moduleName))
-        ).copyInformationFromForTree(n));
-
-        Node moduleExportsProp = IR.getprop(IR.name(moduleName), IR.string("module$exports"));
-        n.addChildToBack(
-            IR.ifNode(moduleExportsProp,
-                IR.block(
-                    IR.exprResult(
-                        IR.assign(IR.name(moduleName), moduleExportsProp.cloneTree())))).
-            copyInformationFromForTree(n)
-        );
-
-        // Rename vars to not conflict in global scope.
-        NodeTraversal.traverse(compiler, n, new SuffixVarsCallback(moduleName));
-
-        compiler.reportCodeChange();
+        visitScript(n);
       }
 
-      if (n.isGetProp() &&
-          n.getChildAtIndex(0).isName() &&
-          "module".equals( n.getChildAtIndex(0).getString()) &&
+      if (n.isGetProp() && n.getChildAtIndex(0).isName() &&
+          "module".equals(n.getChildAtIndex(0).getString()) &&
           n.getChildAtIndex(1).isString() &&
           "exports".equals(n.getChildAtIndex(1).getString())) {
-        String moduleName = guessCJSModuleName(n.getSourceFileName());
-        n.getChildAtIndex(0).setString(moduleName);
-        n.getChildAtIndex(1).setString("module$exports");
+        visitModuleExports(n);
       }
     }
 
-    Node getCurrentScriptNode(Node n) {
+    /**
+     * Visit require calls. Emit corresponding goog.require and rewrite require
+     * to be a direct reference to name of require module.
+     */
+    private void visitRequireCall(NodeTraversal t, Node require, Node parent) {
+      String moduleName = toModuleName(require.getChildAtIndex(1).getString(),
+          normalizeSourceName(t.getSourceName()));
+      Node moduleRef = IR.name(moduleName).srcref(require);
+      parent.replaceChild(require, moduleRef);
+      Node script = getCurrentScriptNode(parent);
+      if (reportDependencies) {
+        compiler.getInput(script.getInputId()).addRequire(moduleName);
+      }
+      // Rewrite require("name").
+      script.addChildToFront(IR.exprResult(
+          IR.call(IR.getprop(IR.name("goog"), IR.string("require")),
+              IR.string(moduleName))).copyInformationFromForTree(require));
+      compiler.reportCodeChange();
+    }
+
+    /**
+     * Emit goog.provide and add suffix to all global vars to avoid conflicts
+     * with other modules.
+     *
+     * @param script
+     */
+    private void visitScript(Node script) {
+      String moduleName = guessCJSModuleName(normalizeSourceName(script
+          .getSourceFileName()));
+      script.addChildToFront(IR.var(IR.name(moduleName), IR.objectlit())
+          .copyInformationFromForTree(script));
+      // System.out.println("Provide " + moduleName);
+      if (reportDependencies) {
+        CompilerInput ci = compiler.getInput(script.getInputId());
+        ci.addProvide(moduleName);
+        JSModule m = new JSModule(moduleName);
+        m.add(ci);
+        module = m;
+      }
+      script.addChildToFront(IR.exprResult(
+          IR.call(IR.getprop(IR.name("goog"), IR.string("provide")),
+              IR.string(moduleName))).copyInformationFromForTree(script));
+
+      emitOptionalModuleExportsOverride(script, moduleName);
+
+      // Rename vars to not conflict in global scope.
+      NodeTraversal.traverse(compiler, script, new SuffixVarsCallback(
+          moduleName));
+
+      compiler.reportCodeChange();
+    }
+
+    /**
+     * Emit <code>if (moduleName.module$exports) {
+     *    moduleName = moduleName.module$export;
+     * }</code> at end of file.
+     */
+    private void emitOptionalModuleExportsOverride(Node script,
+        String moduleName) {
+      Node moduleExportsProp = IR.getprop(IR.name(moduleName),
+          IR.string("module$exports"));
+      script.addChildToBack(IR.ifNode(
+          moduleExportsProp,
+          IR.block(IR.exprResult(IR.assign(IR.name(moduleName),
+              moduleExportsProp.cloneTree())))).copyInformationFromForTree(
+          script));
+    }
+
+    /**
+     * Rewrite module.exports to moduleName.module$exports.
+     */
+    private void visitModuleExports(Node prop) {
+      String moduleName = guessCJSModuleName(prop.getSourceFileName());
+      prop.getChildAtIndex(0).setString(moduleName);
+      prop.getChildAtIndex(1).setString("module$exports");
+    }
+
+    /**
+     * Returns next script node in parents.
+     */
+    private Node getCurrentScriptNode(Node n) {
       while (true) {
         if (n.isScript()) {
           return n;
@@ -194,8 +234,10 @@ class ProcessCommonJSModules implements CompilerPass {
     }
   }
 
-  private class SuffixVarsCallback
-      extends AbstractPostOrderCallback {
+  /**
+   * Traverses a node tree and appends a suffix to all global variable names.
+   */
+  private class SuffixVarsCallback extends AbstractPostOrderCallback {
 
     private final String suffix;
 
--- src/com/google/javascript/jscomp/TransformAMDToCJSModule.java
+++ src/com/google/javascript/jscomp/TransformAMDToCJSModule.java
@@ -15,22 +15,25 @@
  */
 package com.google.javascript.jscomp;
 
-import java.util.List;
-
-import com.google.common.collect.Lists;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
 import com.google.javascript.rhino.IR;
 import com.google.javascript.rhino.Node;
 
 /**
- *
+ * Rewrites an AMD module https://github.com/amdjs/amdjs-api/wiki/AMD to a
+ * Common JS module. See ProcessCommonJSModule.java for follow up processing
+ * step.
  */
 class TransformAMDToCJSModule implements CompilerPass {
 
   private final AbstractCompiler compiler;
 
-  private final DiagnosticType UNSUPPORTED_DEFINE_SIGNATURE = DiagnosticType.error("UNSUPPORTED_DEFINE_SIGNATURE",
-      "Only define(function() ...) and define(['dep', 'dep1'], function(d0, d2, [exports, module]) ...) forms are currently supported.");
+  private final DiagnosticType UNSUPPORTED_DEFINE_SIGNATURE = DiagnosticType
+      .error(
+          "UNSUPPORTED_DEFINE_SIGNATURE",
+          "Only define(function() ...), define(OBJECT_LITERAL) and define("
+              + "['dep', 'dep1'], function(d0, d2, [exports, module]) ...) forms "
+              + "are currently supported.");
 
   TransformAMDToCJSModule(AbstractCompiler compiler) {
     this.compiler = compiler;
@@ -46,125 +49,152 @@ class TransformAMDToCJSModule implements CompilerPass {
   }
 
   private boolean isSpecialModuleName(String moduleName) {
-    return "exports".equals(moduleName) ||
-        "require".equals(moduleName)  ||
+    return "exports".equals(moduleName) || "require".equals(moduleName) ||
         "module".equals(moduleName);
   }
 
   /**
+   * Rewrites calls to define which has to be in void context just below the
+   * current script node.
    */
-  private class TransformAMDModulesCallback
-      extends AbstractPostOrderCallback {
+  private class TransformAMDModulesCallback extends AbstractPostOrderCallback {
 
     @Override
     public void visit(NodeTraversal t, Node n, Node parent) {
       if (n.isCall() && n.getFirstChild() != null &&
-          n.getFirstChild().isName() && "define".equals(n.getFirstChild().getString())) {
+          n.getFirstChild().isName() &&
+          "define".equals(n.getFirstChild().getString()) &&
+          parent.isExprResult() && parent.getParent().isScript()) {
+        Node script = parent.getParent();
         Node requiresNode = null;
         Node callback = null;
-        Node onlyExport = null;
         int defineArity = n.getChildCount() - 1;
         if (defineArity == 0) {
           unsupportedDefineError(t.getSourceName(), n);
           return; // Can't handle this.
-        }
-        else if (defineArity == 1) {
+        } else if (defineArity == 1) {
           callback = n.getChildAtIndex(1);
           if (callback.isObjectLit()) {
-            onlyExport = callback;
-            callback = null;
+            handleDefineObjectLiteral(parent, callback, script);
+            return;
           }
-        }
-        else if (defineArity == 2) {
+        } else if (defineArity == 2) {
           requiresNode = n.getChildAtIndex(1);
           callback = n.getChildAtIndex(2);
-        }
-        else if(defineArity >= 3) {
+        } else if (defineArity >= 3) {
           unsupportedDefineError(t.getSourceName(), n);
           return; // Can't handle this.
         }
 
-        Node context = parent.getParent(); // Removes EXPR_RESULT around define call;
-
-        if (onlyExport != null) {
-          onlyExport.getParent().removeChild(onlyExport);
-          context.replaceChild(parent, IR.exprResult(
-              IR.assign(IR.name("exports"), onlyExport)).copyInformationFromForTree(onlyExport));
-          compiler.reportCodeChange();
-          return;
-        }
-
-        if (!callback.isFunction()) {
-          unsupportedDefineError(t.getSourceName(), n);
-          return; // Can't handle this.
-        }
-        if (requiresNode != null && !requiresNode.isArrayLit()) {
+        if (!callback.isFunction() ||
+            (requiresNode != null && !requiresNode.isArrayLit())) {
           unsupportedDefineError(t.getSourceName(), n);
           return; // Can't handle this.
         }
 
-        int i = 0;
-        Node paramList = callback.getChildAtIndex(1);
-        for (Node aliasNode : paramList.children()) {
-          String moduleName = null;
-          Node modNode = requiresNode != null && requiresNode.getChildCount() > i ?
-              requiresNode.getChildAtIndex(i) :
-              null;
-
-          if (modNode != null) {
-            moduleName = modNode.getString();
-          }
-          if (aliasNode != null) {
-            String aliasName = aliasNode.getString();
-
-            if (isSpecialModuleName(moduleName)) {
-              continue;
-            }
-
-            if (moduleName != null && moduleName.indexOf('!') == -1) { // TODO(malteubl) Handle ?,! in define better.
-              Node call = IR.call(IR.name("require"), IR.string(moduleName));
-              call.putBooleanProp(Node.FREE_CALL, true);
-              context.addChildToFront(
-                  IR.var(IR.name(aliasName), call).copyInformationFromForTree(aliasNode));
-            } else {
-              // ignore exports, require and module (because they are implicit in CJS);
-              if (isSpecialModuleName(aliasName)) {
-                continue;
-              }
-              context.addChildToFront(
-                  IR.var(IR.name(aliasName)).copyInformationFromForTree(aliasNode));
-            }
-          }
-          i++;
-        }
+        handleRequiresAndParamList(script, requiresNode, callback);
 
         Node callbackBlock = callback.getChildAtIndex(2);
+        NodeTraversal.traverse(compiler, callbackBlock,
+            new DefineCallbackReturnCallback());
 
-        NodeTraversal.traverse(compiler, callbackBlock, new NodeTraversal.AbstractShallowCallback() {
-          @Override
-          public void visit(NodeTraversal t, Node n, Node parent) {
-            if (n.isReturn() && n.hasChildren()) {
-              Node retVal = n.getFirstChild();
-              n.removeChild(retVal);
-              parent.replaceChild(n, IR.exprResult(
-                  IR.assign(
-                      IR.getprop(IR.name("module"), IR.string("exports")).copyInformationFromForTree(n),
-                      retVal).copyInformationFrom(n)).copyInformationFrom(n));
-            }
-          }
-        });
-
-        int curIndex = context.getIndexOfChild(parent);
-        context.removeChild(parent);
-        Node before = context.getChildAtIndex(curIndex);
-        for (Node body : callbackBlock.children()) {
-          body.getParent().removeChild(body);
-          if (before != null) {
-            context.addChildBefore(body, before);
-          }
-          context.addChildToBack(body);
-        }
+        moveCallbackContentToTopLevel(parent, script, callbackBlock);
         compiler.reportCodeChange();
+      }
+    }
+
+    /**
+     * When define is called with an object literal, assign it to exports and
+     * we're done.
+     */
+    private void handleDefineObjectLiteral(Node parent, Node onlyExport,
+        Node script) {
+      onlyExport.getParent().removeChild(onlyExport);
+      script.replaceChild(parent,
+          IR.exprResult(IR.assign(IR.name("exports"), onlyExport))
+              .copyInformationFromForTree(onlyExport));
+      compiler.reportCodeChange();
+    }
+
+    /**
+     * Rewrites the required modules to
+     * <code>var nameInParamList = require("nameFromRequireList");</code>
+     */
+    private void handleRequiresAndParamList(Node script, Node requiresNode,
+        Node callback) {
+      int i = 0;
+      Node paramList = callback.getChildAtIndex(1);
+      for (Node aliasNode : paramList.children()) {
+        String moduleName = null;
+        Node modNode = requiresNode != null &&
+            requiresNode.getChildCount() > i ?
+                requiresNode.getChildAtIndex(i) : null;
+
+        if (modNode != null) {
+          moduleName = modNode.getString();
+        }
+        if (aliasNode != null) {
+          String aliasName = aliasNode.getString();
+
+          if (isSpecialModuleName(moduleName)) {
+            continue;
+          }
+
+          // TODO(malteubl) Handle ?,! in define better.
+          if (moduleName != null && moduleName.indexOf('!') == -1) {
+            Node call = IR.call(IR.name("require"), IR.string(moduleName));
+            call.putBooleanProp(Node.FREE_CALL, true);
+            script.addChildToFront(IR.var(IR.name(aliasName), call)
+                .copyInformationFromForTree(aliasNode));
+          } else {
+            // ignore exports, require and module (because they are implicit
+            // in CJS);
+            if (isSpecialModuleName(aliasName)) {
+              continue;
+            }
+            script.addChildToFront(IR.var(IR.name(aliasName))
+                .copyInformationFromForTree(aliasNode));
+          }
+        }
+        i++;
+      }
+    }
+
+    /**
+     * Moves the statements in the callback to be direct children of the
+     * current script.
+     */
+    private void moveCallbackContentToTopLevel(Node parent, Node script,
+        Node callbackBlock) {
+      int curIndex = script.getIndexOfChild(parent);
+      script.removeChild(parent);
+      Node before = script.getChildAtIndex(curIndex);
+      for (Node body : callbackBlock.children()) {
+        body.getParent().removeChild(body);
+        if (before != null) {
+          script.addChildBefore(body, before);
+        }
+        script.addChildToBack(body);
+      }
+    }
+  }
+
+  /**
+   * Rewrites the return statement of the callback to be an assingment to
+   * module.exports.
+   */
+  private class DefineCallbackReturnCallback extends
+      NodeTraversal.AbstractShallowStatementCallback {
+    @Override
+    public void visit(NodeTraversal t, Node n, Node parent) {
+      if (n.isReturn() && n.hasChildren()) {
+        Node retVal = n.getFirstChild();
+        n.removeChild(retVal);
+        parent.replaceChild(n, IR.exprResult(
+            IR.assign(
+                IR.getprop(IR.name("module"), IR.string("exports"))
+                    .copyInformationFromForTree(n), retVal)
+                    .copyInformationFrom(n)).copyInformationFrom(n));
       }
     }
   }
